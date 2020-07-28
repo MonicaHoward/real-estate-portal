4863b8ead79456b9cafedad96e58517066d33049
Monica Howard <mo@Monicas-Air.attlocal.net>
2020-07-28 16:30:09 -0500

    removed default styling and set up file structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => (
+    <div className="home">
+        <div className="home-category">
+            <div className="category">
+                <div className="content">
+                    <h1>Craftsman</h1>
+                    <span>Browse Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Home;
